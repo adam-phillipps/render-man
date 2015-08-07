60232db8115ae873951ca64bb5c84c8d7c1e6fc4
adam-phillipps <adam.phillipps@gmail.com>
2015-08-07 02:47:49 -0600

    WIP add #should_stop to render_slave

--- render_slave.rb
+++ render_slave.rb
@@ -3,7 +3,7 @@ require 'byebug'
 
 class RenderSlave
   def initialize
-    @boot_time = boot_time
+    @refresh_time = boot_time
     creds = Aws::Credentials.new(
       ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
     @s3 = Aws::S3::Client.new(region: ENV['AWS_REGION'],
@@ -18,14 +18,34 @@ class RenderSlave
 
   def boot_time
     # get ec2 boot time
-    @boot_time ||= Time.now.to_i
+    Time.now.to_i
   end
 
   def poll
-    until time_left <= 300 do
+    until should_stop? do
       job = @s3.list_objects(@backlog).objects.first
       move_to_wip_with job.key
       run job
+    end
+  end
+
+  def low_ratio_of_backlog_to_wip?
+    byebug
+    wip = @s3.list_objects(bucket: 'render-wip').contents.count
+    wip = wip == 0 ? 0.01 : wip # guards agains dividing by zero
+    (@s3.list_objects(bucket: 'render-test').contents.count / wip) <= 10.0
+  end
+
+  def should_stop?
+    if time_left < 300
+      if low_ratio_of_backlog_to_wip?
+        true
+      else
+        @refresh_time += 3900 # adds another hour and compensates for the 5 minutes
+        false
+      end
+    else
+      false
     end
   end
 
@@ -35,7 +55,7 @@ class RenderSlave
   end
 
   def time_left
-    3300 - (Time.now - @boot_time)
+    3300 - (Time.now.to_i - @refresh_time)
   end
 
   def move_to_wip_with(key)
--- spec/render_slave_spec.rb
+++ spec/render_slave_spec.rb
@@ -8,4 +8,8 @@ describe RenderSlave do
   it 'should have a time left within 3300 seconds' do
     expect(Subject.time_left).to be <= 3300
   end
+
+  it 'should have valid boot time' do
+    expect(Subject.boot_time).to be_kind_of Number
+  end
 end
