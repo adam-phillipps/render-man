609f15d7ac98d946d0d717453346456b6aa39d86
Adam Phillipps <adam@adam-l.corp.instructure.com>
2015-08-17 11:30:58 -0600

    WIP spot_maker with request fleets ready for testing 8-17-15

--- spot_maker.rb
+++ spot_maker.rb
@@ -5,8 +5,12 @@ require 'byebug'
 
 class SpotMaker
   def initialize
-    @backlog = 'render-test'
-    @wip = 'render-wip'
+    @backlog = Aws::S3::Bucket.new(
+      region: 'us-west-2', credentials: creds, name: 'render-test')
+    @wip = Aws::S3::Bucket.new(
+      region: 'us-west-2', credentials: creds, name: 'render-wip-test')    
+    # @backlog = 'render-test'
+    # @wip = 'render-wip'
     creds = Aws::Credentials.new(
       ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
     @s3 = Aws::S3::Client.new(
@@ -17,12 +21,14 @@ class SpotMaker
   end
 
   def run_program
+    byebug
     ratio = birth_ratio
     start_slaves(appropriate_ratio_for_starting(ratio)) if ratio >= 10
     poll
   end
 
   def poll
+    byebug
     poller = Aws::SQS::QueuePoller.new(
       'https://sqs.us-west-2.amazonaws.com/828660616807/backlog')
     poller.poll do |msg|     
@@ -32,72 +38,37 @@ class SpotMaker
 
   def birth_ratio
     byebug
-    wip = @s3.list_objects(bucket: 'render-wip').contents.count
+    wip = @wip.objects.count
     wip = wip == 0 ? 0.01 : wip # guards agains dividing by zero
-    @s3.list_objects(bucket: 'render-test').contents.count / wip
+    (@backlog.objects.count / wip)
+    # wip = @s3.list_objects(bucket: 'render-wip').contents.count
+    # wip = wip == 0 ? 0.01 : wip # guards agains dividing by zero
+    # @s3.list_objects(bucket: 'render-test').contents.count / wip
+  end
+
+  def appropriate_ratio_for_starting(count)
+    byebug
+    count / 10
   end
 
   def start_slaves(instance_count)
+    byebug
     @ec2.request_spot_fleet(
       spot_fleet_request_config: slave_fleet_params(instance_count)
   end
 
-
-  def render_slave_ami
-    @ec2.describe_images(filters: {name: 'Name', values: 'RenderSlave'}).
-      images.first.image_id
-  end
-
-  def map_for_required_options(spot_prices)
-    byebug
-    spot_prices.each.map(&:spot_price_history).flatten.
-      map{ |sph| {
-        spot_price: sph.spot_price, 
-        availability_zone: sph.availability_zone, 
-        instance_type: sph.instance_type} }.
-          min_by {|sp| sp[:price]}
-  end
-
-  def best_price_and_zone_for(options={})
-    spot_prices = []
-    @ec2.describe_spot_price_history(
-    start_time: (Time.now + 86400).iso8601.to_s,# future date yields current spot prices
-    instance_types: [options[:instance_types]],
-    product_descriptions: 'Windows'])
-    best_match = map_for_required_options(spot_prices)
-    best_match[:spot_price] = add_buffer_to_price(best_match[:spot_price])
-    best_match
-  end
-
-  def add_buffer_to_price(price)
-    float_price = price.to_f
-    ((float_price + (float_price * 0.2)).round(3)).to_s
-  end
-  
-  def all_zones
-    @ec2.describe_availability_zones.
-      availability_zones.map(&:zone_name)
-  end
-
-  def available_instance_types
-    ['t1.micro', 't2.micro', 'm3.large']
-  end
-
   def slave_fleet_params(instance_count)
-    best_match_data = best_price_and_zone_for(
-      instance_type: available_instance_types,
-      product_descriptions: 'Windows'})
-
-    { spot_price: best_match_data[:spot_price],
+    byebug
+    { spot_price: '12.00',
     target_capacity: instance_count,
     iam_fleet_role: 'render-man_fleet_request', # required
-    launch_specifications: spot_fleet_launch_specifications }
+    launch_specifications: slave_fleet_launch_specifications }
   end
 
-  def spot_fleet_launch_specifications
+  def slave_fleet_launch_specifications
+    byebug
     launch_specifications = []
     available_instance_types.each do |inst_type|
-
       launch_specifications << {
         image_id: render_slave_ami,
         key_name: 'RenderSlave',
@@ -107,7 +78,21 @@ class SpotMaker
           arn: 'arn:aws:iam::828660616807:role/render-man_fleet_request',
           name: 'render-man_fleet_request'}}
     end
+    byebug
     launch_specifications
+  end
+
+  def render_slave_ami
+    byebug
+    @ami ||= @ec2.describe_images(filters: { name: 'Name', values: 'RenderSlave' }).
+      images.first.image_id
+    byebug
+  end
+
+  def available_instance_types
+    byebug
+    ['t1.micro', 't2.micro', 'm3.large']
+  end
 end
 
 SpotMaker.new
