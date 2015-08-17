929853c4b955961a1184e9d919e3680a058e577d
adam-phillipps <adam.phillipps@gmail.com>
2015-08-17 08:58:57 -0600

    WIP change spot_maker to use spot fleet requests 8-17-15

--- spot_maker.rb
+++ spot_maker.rb
@@ -17,8 +17,8 @@ class SpotMaker
   end
 
   def run_program
-    ratio = ratio_of_backlog_to_wip
-    start_slaves(ratio / 10) if ratio >= 10
+    ratio = birth_ratio
+    start_slaves(appropriate_ratio_for_starting(ratio)) if ratio >= 10
     poll
   end
 
@@ -30,7 +30,7 @@ class SpotMaker
     end
   end
 
-  def ratio_of_backlog_to_wip
+  def birth_ratio
     byebug
     wip = @s3.list_objects(bucket: 'render-wip').contents.count
     wip = wip == 0 ? 0.01 : wip # guards agains dividing by zero
@@ -39,18 +39,13 @@ class SpotMaker
 
   def start_slaves(instance_count)
     @ec2.request_spot_fleet(
-      slave_fleet_params(instance_count))
+      spot_fleet_request_config: slave_fleet_params(instance_count)
   end
 
-  def slave_fleet_params(instance_count, options={})
-    { spot_price: options[:spot_price],
-    target_capacity: instance_count,
-    launch_specifications: [
-      { image_id: 'ami-9384f7a3',
-      instance_type: 't2.micro' }],
-      iam_instance_profile: {
-        arn: 'arn:aws:iam::828660616807:role/render-man_fleet_request',
-        name: 'render-man_fleet_request'}}
+
+  def render_slave_ami
+    @ec2.describe_images(filters: {name: 'Name', values: 'RenderSlave'}).
+      images.first.image_id
   end
 
   def map_for_required_options(spot_prices)
@@ -65,13 +60,10 @@ class SpotMaker
 
   def best_price_and_zone_for(options={})
     spot_prices = []
-    all_zones.each do |az|
-      spot_prices << @ec2.describe_spot_price_history(
-      start_time: (Time.now - 86400).iso8601.to_s,
-      instance_types: [options[:instance_types]],
-      product_descriptions: [options[:product_description]],
-      availability_zone: az)
-    end
+    @ec2.describe_spot_price_history(
+    start_time: (Time.now + 86400).iso8601.to_s,# future date yields current spot prices
+    instance_types: [options[:instance_types]],
+    product_descriptions: 'Windows'])
     best_match = map_for_required_options(spot_prices)
     best_match[:spot_price] = add_buffer_to_price(best_match[:spot_price])
     best_match
@@ -86,6 +78,36 @@ class SpotMaker
     @ec2.describe_availability_zones.
       availability_zones.map(&:zone_name)
   end
+
+  def available_instance_types
+    ['t1.micro', 't2.micro', 'm3.large']
+  end
+
+  def slave_fleet_params(instance_count)
+    best_match_data = best_price_and_zone_for(
+      instance_type: available_instance_types,
+      product_descriptions: 'Windows'})
+
+    { spot_price: best_match_data[:spot_price],
+    target_capacity: instance_count,
+    iam_fleet_role: 'render-man_fleet_request', # required
+    launch_specifications: spot_fleet_launch_specifications }
+  end
+
+  def spot_fleet_launch_specifications
+    launch_specifications = []
+    available_instance_types.each do |inst_type|
+
+      launch_specifications << {
+        image_id: render_slave_ami,
+        key_name: 'RenderSlave',
+        instance_type: inst_type,
+        monitoring: { enabled: true },
+        iam_instance_profile: {
+          arn: 'arn:aws:iam::828660616807:role/render-man_fleet_request',
+          name: 'render-man_fleet_request'}}
+    end
+    launch_specifications
 end
 
 SpotMaker.new
