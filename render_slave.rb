25f22120a1a9c5463640acf5bca0fc4afafd62aa
adam-phillipps <adam.phillipps@gmail.com>
2015-09-04 09:48:28 -0600

    add idompotency and termination flags to spot_fleet_request

--- render_slave.rb
+++ render_slave.rb
@@ -1,92 +1,138 @@
 require 'dotenv'
 Dotenv.load
 require 'aws-sdk'
-require 'httpparty'
+require 'httparty'
+require 'byebug'
 
 class RenderSlave
   def initialize
-    @id = HTTParty.get('http://169.254.169.254/latest/meta-data/instance-id')
-    @boot_time = boot_time
-    creds = Aws::Credentials.new(
-      ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-    @s3 = Aws::S3::Client.new(region: ENV['AWS_REGION'],
-      credentials: creds)
-    @s3_resource = Aws::S3::Resource.new(region: ENV['AWS_REGION'],
-      credentials: creds)
-    @ec2 = Aws::EC2::Client.new(region: ENV['AWS_REGION'],
-      credentials: creds)
-    @backlog = Aws::S3::Bucket.new(
-      region: 'us-west-2', credentials: creds, name: 'render-test')
-    @wip = Aws::S3::Bucket.new(
-      region: 'us-west-2', credentials: creds, name: 'render-wip-test')
-    @finished = Aws::S3::Bucket.new(
-      region: 'us-west-2', credentials: creds, name: 'render-finished-test')
+    byebug
     poll
-  end
-
-  def boot_time
-    @boot_time ||= @ec2.describe_instances(instance_ids:[@id]).reservations[0].instances[0].launch_time
   end
 
   def poll
     until should_stop? do
-      sleep rand(571) / 137.0 # offsets polling for other slaves
-      job = @backlog.objects.first.object # get the oldest file
-      unless job_in_wip?(job)
-        job = move_to(job, @backlog, @wip)
-        run job
+      sleep rand(571) / 137.0
+      # could add message_attribute_names to gather namespaced messages
+      # from the queue
+      backlog_poller.poll(
+        default_wait_time: nil,
+        max_number_of_messages: 1,
+        visibility_timeout: 900
+      ) do |msg|
+        begin
+          update_status(wip_address, msg)
+          run_job(msg)
+          update_status(finished_address, msg)
+        rescue
+        # if there's an error during job processing,
+        # the message becomes available again
+          throw :skip_delete
+        end
       end
     end
-    @ec2.terminate_instancers(ids: [@id])
+    ec2.terminate_instancers(ids: [self_id])
   end
 
   def should_stop?
     if hour_mark_approaches?
-      if death_ratio_acheived?
-        true
-      else
-        false
-      end
+      death_ratio_acheived? ? true : false
     else
       false
     end
   end
 
-  def job_in_wip?(job)
-    begin
-      @wip.object(job.key).exists?
-    rescue Aws::S3::Errors::NotFound
-      puts 'nil job, not moving to wip'
-      false
-    end
-  end
-
   def hour_mark_approaches?
-    ((Time.now.to_i - @boot_time) % 3600) > 3300
+    ((Time.now.to_i - boot_time) % 3600) > 3300
   end
 
   def death_ratio_acheived?
-    wip = @wip.objects.count
-    wip = wip == 0 ? 0.01 : wip # guards agains dividing by zero
-    (@backlog.objects.count / wip) <= 10.0
+    death_ratio >= 10
   end
 
-  def run(job)
-    # run the render job
-    sleep 5
-    move_to(job, @wip, @finished)
+  def death_ratio
+    counts = [finished_address, wip_address].map do |board|
+      sqs.get_queue_attributes(
+        queue_url: board,
+        attribute_names: ['ApproximateNumberOfMessages']
+      ).attributes['ApproximateNumberOfMessages'].to_f
+    end
+
+    wip = counts[1]
+    wip = wip == 0.0 ? 1.0 : wip # guards against dividing by zero
+    counts[0] / wip
+  end
+  
+  # should the message and/or file be renamed now?
+  def update_status(board, message)
+    sqs.send_message({
+      queue_url: board,
+      message_body: message.body,
+      message_attributes: message.attributes
+    })
   end
 
-  def move_to(job,source,target)
-    key = job.key
-    @s3.copy_object(
-      bucket: target.name,
-      copy_source: "#{source.name}/#{key}",
-      key: "#{key}")
-    @s3.delete_object(
-      bucket: source.name,
-      key: key)
-    target.object key
+  def run_job(msg)
+    byebug  
+    file = s3.get_object(
+      response_target: '/path/to/file',
+      bucket: 'backlogtester', # customer in
+      key: 'object-key'
+    )
+    start_rendiering(file)
+    File.open('C:\\', 'rb') do |file|
+      s3.put_object(bucket: 'wiptest2', key: '', body: file)
+    end
+  end
+
+  def backlog_poller
+    @backlog_poller ||= Aws::SQS::QueuePoller.new(backlog_address)
+  end
+
+  def sqs
+    @s3 ||= Aws::S3::Client.new(
+      region: ENV['AWS_REGION'],
+      credentials: creds
+    )
+  end
+
+  def start_rendering(file)
+    sleep 10
+  end
+
+  def backlog_address
+    'https://sqs.us-west-2.amazonaws.com/088617881078/backlog_smashanalytics_sqs'
+  end
+
+  def sqs
+    @sqs ||= Aws::SQS::Client.new(credentials: creds)
+  end
+
+  def wip_address
+    'https://sqs.us-west-2.amazonaws.com/088617881078/wip_smashanalytics_sqs'
+  end
+
+  def finished_address
+    'https://sqs.us-west-2.amazonaws.com/088617881078/finished_smashanalytics_sqs'
+  end
+
+  def boot_time
+    @boot_time ||= Time.now.to_i#ec2.describe_instances(instance_ids:[@id]).reservations[0].instances[0].launch_time
+  end
+
+  def self_id
+    'adsflkj'
+    # @id ||= HTTParty.get('http://169.254.169.254/latest/meta-data/instance-id')
+  end
+
+  def ec2
+    @ec2 ||= Aws::EC2::Client.new(region: ENV['AWS_REGION'],
+      credentials: creds)
+  end
+
+  def creds
+    @creds ||= Aws::Credentials.new(
+      ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
   end
 end
 
--- spot_maker.rb
+++ spot_maker.rb
@@ -2,84 +2,146 @@ require 'dotenv'
 Dotenv.load
 require 'aws-sdk'
 require 'date'
+require 'byebug'
+require 'securerandom'
 
 class SpotMaker
   begin
     def initialize
-      creds = Aws::Credentials.new(
-        ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-      @backlog = Aws::S3::Bucket.new(
-        region: 'us-west-2', credentials: creds, name: 'render-test')
-      @wip = Aws::S3::Bucket.new(
-        region: 'us-west-2', credentials: creds, name: 'render-wip-test')    
-      @s3 = Aws::S3::Client.new(
-        region: ENV['AWS_REGION'], credentials: creds)
-      @ec2 = Aws::EC2::Client.new(
-        region: ENV['AWS_REGION'], credentials: creds)
-      @ami_id = @ec2.describe_images(filters: [
-        { name: 'tag:Name', values: ['RenderSlave-prod'] }]).
-          images.first.image_id
-      @ami_id ||= 'ami-875041b7'
-      @spot_fleet_request_ids = []
       poll
     end
 
-    def poll
-      poller = Aws::SQS::QueuePoller.new(
-        'https://sqs.us-west-2.amazonaws.com/828660616807/backlog_test')
-      poller.poll do |msg|
-        poller.delete_message(msg) 
-        run_program
+     def poll
+      loop do
+        sleep 5
+        run_program(adjusted_birth_ratio) if birth_ratio_acheived? || true
       end
     end
 
-    def run_program
-      ratio = birth_ratio
-      start_slaves(appropriate_ratio_for_starting(ratio)) if ratio >= 1#0
-      poll
+    def birth_ratio_acheived?
+      birth_ratio >= 10
     end
 
     def birth_ratio
-      wip = @wip.objects.count.to_f
-      wip = wip == 0 ? 1.0 : wip # guards agains dividing by zero
-      (@backlog.objects.count.to_f / wip)
+      counts = [backlog_address, wip_address].map do |board|
+        sqs.get_queue_attributes(
+          queue_url: board,
+          attribute_names: ['ApproximateNumberOfMessages']
+        ).attributes['ApproximateNumberOfMessages'].to_f
+      end
+
+      wip = counts[1]
+      wip = wip == 0.0 ? 1.0 : wip # guards against dividing by zero
+      counts[0] / wip
     end
 
-    def start_slaves(instance_count)      
-      fleet = @ec2.request_spot_fleet(
-        spot_fleet_request_config: slave_fleet_params(instance_count))
-      # get instanct numbers or request numbers and store them
-    end
-
-    def appropriate_ratio_for_starting(ratio)
-      adjusted_ratio = (ratio / 10.0).floor
+    def adjusted_birth_ratio
+      adjusted_ratio = (birth_ratio / 10.0).floor
       adjusted_ratio == 0 ? 1 : adjusted_ratio
     end
 
+    def run_program(desired_instance_count)
+      # ec2.request_spot_fleet(
+      #   spot_fleet_request_config: slave_fleet_params(desired_instance_count)
+      # )
+      poll
+    end
+
+    # doesn't cache the values in case tags change on the ami, which allows us to change configuration
+    # in the aws console.
     def slave_fleet_params(instance_count)
-      params = {
+      bp = best_price # only want the method to run once
+      price = bp[:spot_price] 
+      availability_zone = bp[:availability_zone]
+      {
         client_token: "RenderSlave-#{SecureRandom.hex}",
-        spot_price: '0.12',
+        spot_price: price, # '0.12',
         target_capacity: instance_count,
         terminate_instances_with_expiration: true,
         iam_fleet_role: 'arn:aws:iam::828660616807:role/render-man_fleet_request',
-        launch_specifications: slave_fleet_launch_specifications }
+        launch_specifications: slave_fleet_launch_specifications(availability_zone)
+      }
     end
 
-    def slave_fleet_launch_specifications
-      launch_specifications = []
-      available_instance_types.each do |inst_type|
-        launch_specifications << {
-          image_id: @ami_id,
+    def best_price(image = slave_image)
+      best_match = spot_prices.each.map(&:spot_price_history).flatten.map do |sph|
+                                                                                                        { 
+                                                                                                          spot_price: sph.spot_price,
+                                                                                                          availability_zone: sph.availability_zone,
+                                                                                                          instance_type: sph.instance_type 
+                                                                                                        }
+                                                                                                      end.min_by { |sp| sp[:price] }
+
+      # make sure this markup in max spot price is wanted
+      best_match[:spot_price] = (best_match[:spot_price].to_f + (best_match[:spot_price].to_f*0.15)).round(3).to_s
+      best_match
+    end
+
+    def spot_prices
+      @spot_prices = []
+      if @spot_prices.empty?
+        availability_zones.each do |az|
+          @spot_prices << ec2.describe_spot_price_history(spot_price_history_params(az))
+        end
+      end
+      @spot_prices # this is a hash with the availability zone, instance type and recommended bid
+    end
+
+    # not caching for the same reason as slave_fleet_params (allows config via aws console via ami tags)
+    def slave_fleet_launch_specifications(availability_zone)
+      slave_image_tag_filter('instance_types').map do |inst_type|
+        {
+          image_id: slave_image.image_id,
           key_name: 'RenderSlave',
           instance_type: inst_type,
-          monitoring: { enabled: true }}
+          monitoring: { enabled: true },
+          placement: { availability_zone: availability_zone }
+        }
       end
-      launch_specifications
     end
 
-    def available_instance_types
-      ['m3.medium', 'm3.large']
+    def slave_image_tag_filter(tag_name)
+      byebug
+      slave_image.tags.find { |t| t.key.include?(tag_name) }.value.split(',')
+    end
+
+    def slave_image
+      @slave_image ||= ec2.describe_images(
+        filters: [{ name: 'tag:Name', values: ['RenderSlave'] }]
+      ).images.first
+    end
+
+    def availability_zones
+      @az ||= ec2.describe_availability_zones.availability_zones.map(&:zone_name)
+    end
+
+    def spot_price_history_params(availability_zone)
+        { 
+          start_time: (Time.now + 36000).iso8601.to_s,
+          instance_types: slave_image_tag_filter('instance_types'),#s.select { |t| t.key.eql?  }.first.value.split(','),
+          product_descriptions: slave_image_tag_filter('product_descriptions'),#slave_image.tags.select{ |t| t.key.eql? 'product_descriptions' }.first.value.split(','),
+          availability_zone: availability_zone
+        }
+    end
+
+    def creds
+      @creds ||= Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+    end
+
+    def ec2
+      @ec2 ||= Aws::EC2::Client.new(region: 'us-west-2', credentials: creds)
+    end
+
+    def sqs
+      @sqs ||= Aws::SQS::Client.new(credentials: creds)
+    end
+
+    def backlog_address
+      'https://sqs.us-west-2.amazonaws.com/088617881078/backlog_smashanalytics_sqs'
+    end
+
+    def wip_address
+      'https://sqs.us-west-2.amazonaws.com/088617881078/wip_smashanalytics_sqs'
     end
 
   rescue => e
